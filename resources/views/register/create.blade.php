8980bcc96e703fbad898df1faa76dca8f4f4f3b0
Moose <mostafasaid1994@gmail.com>
2021-12-26 16:45:43 +0200

    creating routes for register page

---
+++ resources/views/register/create.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <section class="px-6 py-8">
+        <main class="max-w-lg mx-auto">
+
+            <form method="POST" action="/register">
+
+
+
+            </form>
+        </main>
+    </section>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+route::get('register', [RegisterController::class, 'create']);
