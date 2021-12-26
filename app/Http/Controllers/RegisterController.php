e751f859d84ec40572c18277c5b4573e6e84f57a
Moose <mostafasaid1994@gmail.com>
2021-12-26 17:41:41 +0200

    errors and unique values

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -16,8 +16,8 @@ class RegisterController extends Controller
     {
         $attributes = request()->validate([
             'name' => 'required|max:255|min:3',
-            'username' => 'required|max:255|min:3',
-            'email' => 'required|email|max:255',
+            'username' => 'required|max:255|min:3|unique:users,username',
+            'email' => 'required|email|max:255|unique:users,email',
             'password' => 'required|max:255|min:3'
         ]);
 
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -10,7 +10,12 @@
                         Name
                     </label>
 
-                    <input class="border border-gray-400 p-2 w-full" type="text" name="name" id="name" required>
+                    <input class="border border-gray-400 p-2 w-full" type="text" name="name" id="name"
+                        value="{{ old('name') }}" required>
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
 
                 </div>
                 <div class="mb-6">
@@ -19,7 +24,12 @@
                         Username
                     </label>
 
-                    <input class="border border-gray-400 p-2 w-full" type="text" name="username" id="username" required>
+                    <input class="border border-gray-400 p-2 w-full" type="text" name="username" id="username"
+                        value="{{ old('username') }}" required>
+
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
 
                 </div>
                 <div class="mb-6">
@@ -28,7 +38,12 @@
                         E-Mail
                     </label>
 
-                    <input class="border border-gray-400 p-2 w-full" type="email" name="email" id="email" required>
+                    <input class="border border-gray-400 p-2 w-full" type="email" name="email" id="email"
+                        value="{{ old('email') }}" required>
+
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
 
                 </div>
                 <div class="mb-6">
@@ -39,6 +54,10 @@
 
                     <input class="border border-gray-400 p-2 w-full" type="password" name="password" id="password"
                         required>
+
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
 
                 </div>
                 <div class="mb-6">
